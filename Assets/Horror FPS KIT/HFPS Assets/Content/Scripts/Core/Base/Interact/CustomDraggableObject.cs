e6be32e3af7ef3ca130b44a38dbecc06cc08317f
miquel <miquel_giru_96@hotmail.com>
2022-10-25 17:10:22 +0200

    upload game pt 4

---
+++ Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/Core/Base/Interact/CustomDraggableObject.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * DraggableObject.cs - by ThunderWire Studio
+ * ver. 1.0
+*/
+
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using WaterBuoyancy;
+using WaterBuoyancy.Collections;
+
+namespace HFPS.Systems
+{
+    /// <summary>
+    /// Script defining Draggable Objects.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody), typeof(Collider))]
+    public class CustomDraggableObject : DraggableObject
+    {
+
+
+        private void Start()
+        {
+            enableWaterBuoyancy = false;
+            enableWaterFoam = false;
+            calculateDensity = false;
+        }
+
+    }
+
+
+    
+}
---
+++ Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/Core/Base/Interact/GrabInteractiveItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+using ThunderWire.Utility;
+using HFPS.UI;
+
+#if TW_LOCALIZATION_PRESENT
+using ThunderWire.Localization;
+#endif
+
+namespace HFPS.Systems
+{
+    public class GrabInteractiveItem : InteractiveItem
+    {
+
+        private Vector3 originalScale;
+        public Vector3 grabScale;
+        private Transform placeHolder;
+
+
+        public bool AllowExamine;
+
+        public override void Awake()
+        {
+            SetCustomConfig();
+            base.Awake();
+            originalScale = transform.localScale;
+        }
+
+
+        private void SetCustomConfig()
+        {
+            //settings
+            itemType = ItemType.GenericItem;
+            examineType = ExamineType.Object;
+            messageType = MessageType.None;
+            disableType = DisableType.None;
+
+            //item settings
+            interactShowTitle = false;
+            floatingIcon = true;
+
+            //examine settings
+            allowExamineTake = true;
+        }
+
+        public override void UseObject()
+        {
+            if(itemType == ItemType.GrabItem)
+            {
+                if(placeHolder == null)
+                    placeHolder = Camera.main.transform.parent.GetChild(2);
+
+                transform.SetParent(placeHolder, false);
+                transform.localScale = grabScale;
+            }
+        }
+    }
+}
